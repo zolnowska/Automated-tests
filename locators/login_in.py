e7807110d18c9da93907c7dad04bcde99a78c374
zolnowska <sabina.zolnowska@gmail.com>
2020-10-06 15:53:31 +0200

    delete files that have changed positions

--- locators/login_in.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-INPUT_EMAIL = (By.XPATH, '//input[@class="form-control"]')
-INPUT_PASSWORD = (By.XPATH, '//input[@type="password"]')
-BTN_SIGN_IN = (By.ID, 'submit-login')
-BTN_CREATE_ACCOUNT = (By.XPATH, '//div[@class="no-account"]/a')
-TEXT_AUTHENTICATION_FAILED = (By.XPATH, '//div[@class="help-block"]')
--- locators/order_confirmation.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-TEXT_1ST_BOUGHT_ITEM = (By.XPATH, '//div[@class="col-sm-4 col-xs-9 details"]/span')
-TEXT_ITEM_COST_1ST_ITEM = (By.XPATH, '//div[@class="col-xs-5 text-sm-right text-xs-left"]')
-TEXT_QUANTITY_1ST_ITEMS = (By.XPATH, '//div[@class="col-xs-2"]')
-TEXT_ITEMS_COST_1ST_ITEMS = (By.XPATH, '//div[@class="col-xs-5 text-xs-right bold"]')
-TEXT_SUBTOTAL_COST = (By.XPATH, '//*[@id="order-items"]/div/table/tbody/tr[1]/td[2]')
-TEXT_SHIPPING_COST = (By.XPATH, '//*[@id="order-items"]/div/table/tbody/tr[2]/td[2]')
-TEXT_TOTAL_COST = (By.XPATH, '//*[@id="order-items"]/div/table/tbody/tr[3]/td[2]')
