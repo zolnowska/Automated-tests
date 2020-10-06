f26088a3f82e2df68323b80d9375ade19538861a
zolnowska <sabina.zolnowska@gmail.com>
2020-10-06 15:53:31 +0200

    * Add file requirements.txt * Use Faker library to generate random data * Create class for Locators of every page and place in same file with class responsible for actions on this locators * Create Class with URLs and place them in tests folder * Output of tests like Screenshots will be saved in directory reports

--- locators/checkout/cart.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-BTN_PROCEED_TO_CHECKOUT = (By.XPATH, '//div[@class="text-sm-center"]')
-
-
-
--- locators/product_list/accessories_stationery.py
+++
@@ -1,3 +0,0 @@
-from selenium.webdriver.common.by import By
-
-BTN_MOUNTAIN_FOX_NOTEBOOK = (By.XPATH, '//*[@id="js-product-list"]/div[1]/article[1]/div/a')
